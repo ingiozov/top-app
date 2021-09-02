07c0330bd6b0228d86d25ff13665c6df8457df2e
ingiozov <ingiozov@noreply.github.com>
2021-09-02 16:15:18 +0300

    getStaticProps added to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import { GetStaticProps } from 'next'
+import React, { useState } from 'react'
 import { Button, Htag, P, Rating, Tag } from '../components'
+import { withLayout } from '../layout/Layout'
+import axios from 'axios'
+import { MenuItem } from '../interfaces/menu.interface'
 
-export default function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
   const [rating, setRating] = useState<number>(4)
 
   return (
@@ -23,4 +27,28 @@ export default function Home(): JSX.Element {
       <Rating rating={rating} isEditable setRating={setRating} />
     </>
   )
+}
+
+export default withLayout(Home)
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
+    {
+      firstCategory,
+    }
+  )
+
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  }
+}
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[]
+  firstCategory: number
 }
